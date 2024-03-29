a83b45e6ef270b679252265f682fd976d4db722a
Tutt2004 <thaitux101234@gmail.com>
2024-03-29 07:56:56 +0700

    sang nay code

--- views/layouts/master.php
+++ views/layouts/master.php
@@ -205,14 +205,7 @@
 	<?php require_once PATH_VIEW_USER . '/partials/footer.php' ?>
 
 
-	<a href="#" class="backtop" title="Lên đầu trang">
-		<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="angle-up" role="img"
-			xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-angle-up fa-w-10">
-			<path fill="currentColor"
-				d="M168.5 164.2l148 146.8c4.7 4.7 4.7 12.3 0 17l-19.8 19.8c-4.7 4.7-12.3 4.7-17 0L160 229.3 40.3 347.8c-4.7 4.7-12.3 4.7-17 0L3.5 328c-4.7-4.7-4.7-12.3 0-17l148-146.8c4.7-4.7 12.3-4.7 17 0z"
-				class=""></path>
-		</svg>
-	</a>
+	<?php require_once PATH_VIEW_USER . '/partials/backtop.php' ?>
 	<link rel="preload" as="style"
 		href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/ajaxcart.scss02fb.css?1709175143725"
 		type="text/css">
