4bf59f4346e17e266e655f23716086d88d57ae02
Tutt2004 <thaitux101234@gmail.com>
2024-04-08 08:58:30 +0700

    sua lai link sang ngay 8/4

--- views/styles/thongtintk.php
+++ views/styles/thongtintk.php
@@ -1,20 +1,10 @@
-
-
 <link rel="preload" as='style' type="text/css"
     href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/main.scss.css?1711356971279">
 
-<link rel="preload" as='style' type="text/css"
-    href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/bootstrap-4-3-min.css?1711356971279">
-<link rel="preload" as='style' type="text/css"
-    href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/swiper.scss.css?1711356971279">
-<link rel="stylesheet"
-    href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/bootstrap-4-3-min.css?1711356971279">
+
 <link href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/main.scss.css?1711356971279"
     rel="stylesheet" type="text/css" media="all" />
 
-<link
-    href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/swiper.scss.css?1711356971279"
-    rel="stylesheet" type="text/css" media="all" />
 
 
 <link
@@ -36,47 +26,3 @@
 <link
     href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/account_oder_style.scss.css?1711356971279"
     rel="stylesheet" type="text/css" media="all" />
-
-
-
-
-
-
-<script src="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/stats.min.js?v=f021d1e"></script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-<script src="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/assets/themes_support/bizweb-api.js"
-    type="text/javascript"></script>
-<script src="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/assets/themes_support/common.js"
-    type="text/javascript"></script>
-<script src="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/assets/themes_support/customer.js"
-    type="text/javascript"></script>
-
-
-
-<link rel="preload" as="script"
-    href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/quickview.js?1711356971279" />
-<script type="text/javascript" defer
-    src="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/quickview.js?1711356971279"></script>
-<link rel="preload" as="script"
-    href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/main.js?1711356971279" />
-
-<link rel="preload" as="script"
-    href="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/placeholdertypewriter.js?1711356971279" />
-<script
-    src="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/placeholdertypewriter.js?1711356971279"
-    type="text/javascript"></script>
-
-<script src="<?= BASE_URL ?>assets/client/bizweb.dktcdn.net/100/503/826/themes/932476/assets/main.js?1711356971279"
-    type="text/javascript"></script>
